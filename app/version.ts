6983783721bc3f0576e6107a01ab238956173cc9
erereck <131626550+erereck@users.noreply.github.com>
2026-08-24 09:06:34 -0300

    chore: bump Futbobo to v93.8

--- app/version.ts
+++ app/version.ts
@@ -1,2 +1,2 @@
-export const FUTBOBO_VERSION = "v93.7";
-export const FUTBOBO_VERSION_NAME = "ZONA MISTA & QUADRA";
+export const FUTBOBO_VERSION = "v93.8";
+export const FUTBOBO_VERSION_NAME = "NOMES DO MUNDO";
