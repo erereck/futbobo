1ce4c67d7e25cf784e699498c875d3d773589142
erereck <131626550+erereck@users.noreply.github.com>
2026-08-29 08:11:59 -0300

    feat: load expanded second divisions

--- app/components/shell/FutboboShellWithNames.tsx
+++ app/components/shell/FutboboShellWithNames.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { installExpandedPlayerNames } from "../../career/player-name-expansion";
+import { installSecondDivisions } from "../../career/second-divisions";
 import FutboboShell from "./FutboboShell";
 
 export default function FutboboShellWithNames() {
   installExpandedPlayerNames();
+  installSecondDivisions();
   return <FutboboShell />;
 }
